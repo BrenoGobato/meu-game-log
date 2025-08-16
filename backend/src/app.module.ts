8140e104728afd176da6dbb68e6b9eb7cb64074e
Breno Gobato <bmgobato@gmail.com>
2025-08-16 15:26:39 -0300

    chore(backend): prepare app for production deployment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,22 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'; // 1. Importar
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GamesModule } from './games/games.module'; // NestJS já deve ter adicionado isso
+import { GamesModule } from './games/games.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(), // 2. Carregar as variáveis de ambiente
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '102307', // Garanta que sua senha correta está aqui
-      database: 'postgres',
-      autoLoadEntities: true, // <<<--- ESTA É A LINHA QUE PRECISAMOS ADICIONAR
-      synchronize: true,
+      // 3. Usa a URL do banco de dados da Render (se existir)
+      url: process.env.DATABASE_URL,
+      // 4. Configuração SSL obrigatória para a Render
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      autoLoadEntities: true,
+      synchronize: true, // Manteremos true para facilitar o deploy inicial
     }),
-    GamesModule, // O módulo de Games que criamos
+    GamesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
